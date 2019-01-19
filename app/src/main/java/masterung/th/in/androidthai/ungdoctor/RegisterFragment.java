812ba719e0e90bac433454e222f0b7e6109c8979
masterUNG <phrombutr@gmail.com>
2019-01-19 13:50:03 +0700

    Check Space Success

--- app/src/main/java/masterung/th/in/androidthai/ungdoctor/RegisterFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungdoctor/RegisterFragment.java
@@ -2,10 +2,16 @@ package masterung.th.in.androidthai.ungdoctor;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -13,11 +19,74 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+    //    Explicit
+    private String nameString, surnameString, genderString,
+            heightString, weightString, ageString, userString, passwordString;
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.new_register));
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        setHasOptionsMenu(true);
+
+
+    }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            checkValue();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+
+//        Get Value From EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText surnameEditText = getView().findViewById(R.id.edtSurName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        if (nameString.isEmpty() || surnameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+            myAlert.normalDialog("Have Space", "Please Fill Every Blank");
+        }
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
